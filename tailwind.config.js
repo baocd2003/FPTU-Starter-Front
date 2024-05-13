8f012b644887c986d306ea29f357932857b1c769
diemyolo <quydiem2015@gmail.com>
2024-05-13 22:27:47 +0700

    Add Login + Register page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: (theme) => ({
+        "screen/2": "50vh",
+        "screen/4*3": "calc(100vh / 4 * 3)",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
+      width: (theme) => ({
+        "screen/2": "50vh",
+        "screen/4*3": "calc(100vh / 4 * 3)",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
+      screens: (theme) => ({
+        xxs: "0px",
+      }),
+    },
   },
+  variants: { display: ["responsive", "hover", "focus"] },
   plugins: [],
 };
